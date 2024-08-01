c6ccac8b5b0a58814672b2c46a62d0c68e3621b6
MutukuMnati <skyscraper2486@gmail.com>
2024-08-01 05:39:43 -0700

    Delete initiate.php

--- initiate.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data = json_decode(file_get_contents('php://input'), true);
-
-// Log the data or process it as needed
-file_put_contents('confirmation_log.txt', print_r($data, true), FILE_APPEND);
-
-header('Content-Type: application/json');
-echo json_encode(['ResultCode' => 0, 'ResultDesc' => 'Accepted']);
-?>
